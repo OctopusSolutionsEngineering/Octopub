8205182a63728bc5686acb0cc91788f6e1220d52
Matthew Casperson <matthewcasperson@gmail.com>
2026-02-27 05:14:45 +1000

    Generate unique resource group and web app names to avoid conflicts

--- azure/webapp-container/deploy.sh
+++ azure/webapp-container/deploy.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# Web app names must be globally unique. Resource group names also need to be unique.
+# So we generate a random suffix to append to the names to avoid conflicts with existing resources in the subscription.
 RANDOM_SUFFIX=$(openssl rand -hex 5)
 
+# Set the variables for the deployment. You can override these by passing in parameters when you run the script,
+# or just let it generate unique names for you.
 REGION=${1:-"australiaeast"}
-RG_NAME=${2:-"octopub"}
+RG_NAME=${2:-"octopub-${RANDOM_SUFFIX}"}
 WEBAPP_NAME=${3:-"octopub-${RANDOM_SUFFIX}"}
 HOSTING_PLAN_NAME=${4:-"ASP-${WEBAPP_NAME}"}
 
+# Start by creating the resource group
 az deployment sub create \
   --location ${REGION} \
   --template-file ../resource-group/template.json \
   --parameters ../resource-group/parameters.json \
   --parameters rgName=${RG_NAME}
 
+# Then deploy the web app and hosting plan into the resource group
 az deployment group create \
   --resource-group ${RG_NAME} \
   --template-file template.json \
