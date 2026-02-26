eb3c5de69009b945c0b6fb046d5db8884745769a
Matthew Casperson <matthewcasperson@gmail.com>
2026-02-27 06:26:00 +1000

    Update storage account name format in deployment script for improved compliance

--- azure/function-app/deploy.sh
+++ azure/function-app/deploy.sh
@@ -12,7 +12,7 @@ RG_NAME=${3:-"octopub-${ENVIRONMENT}-${RANDOM_SUFFIX}"}
 FUNCTION_NAME=${4:-"octopub-function-${ENVIRONMENT}-${RANDOM_SUFFIX}"}
 HOSTING_PLAN_NAME=${5:-"ASP-${FUNCTION_NAME}"}
 # Must be lowercase letters and number only, between 3 and 24 characters
-STORAGE_ACCOUNT_NAME=${6:-"octopubfunction${ENVIRONMENT}${RANDOM_SUFFIX}"}
+STORAGE_ACCOUNT_NAME=${6:-"${ENVIRONMENT}${RANDOM_SUFFIX}"}
 
 # Start by creating the resource group
 az deployment sub create \
